1e9ad12639f42ec79c06935a7118cf5d7d06759d
controlvee <adam.goerlich@gmail.com>
2021-02-12 10:06:50 -0600

    Add project files.

---
+++ ControlVee.Port.LINQ/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ControlVee.Port.LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Test
+    {
+        public int I { get; set; }
+        public string S { get; set; }
+        public double D { get; set; }
+    }
+}
