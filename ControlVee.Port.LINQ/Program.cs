65863a9872d084e497a4d1381dce9f8b484b684c
controlvee <adam.goerlich@gmail.com>
2021-02-12 10:27:19 -0600

    .

--- ControlVee.Port.LINQ/Program.cs
+++ ControlVee.Port.LINQ/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ControlVee.Port.LINQ
 {
@@ -8,8 +9,76 @@ namespace ControlVee.Port.LINQ
         static void Main(string[] args)
         {
             int[] arrayOfInt = new int[] { 0,1,2,3,4,5,6,7,8,9 };
-            List<string> listOfString = new List<string>();
-            Dictionary<int, Test> dictionaryOfTest = new Dictionary<int, Test>();
+            List<string> listOfString = new List<string>()
+            {
+                "zero",
+                "one",
+                "two",
+                "three",
+                "four",
+                "five",
+                "six",
+                "seven",
+                "eight",
+                "nine"
+            };
+            List<Test> listOfTest = new List<Test>()
+            {
+                new Test(){ I = 0, S = "zero",  D = 0.0001d },
+                new Test(){ I = 1, S = "one",   D = 1.1111d },
+                new Test(){ I = 2, S = "two",   D = 2.2222d },
+                new Test(){ I = 3, S = "three", D = 3.3333d },
+                new Test(){ I = 4, S = "four",  D = 4.4444d },
+                new Test(){ I = 5, S = "five",  D = 5.5555d },
+                new Test(){ I = 6, S = "six",   D = 6.6666d },
+                new Test(){ I = 7, S = "seven", D = 7.7777d },
+                new Test(){ I = 8, S = "eight", D = 8.8888d },
+                new Test(){ I = 9, S = "nine",  D = 9.9999d },
+            };
+            Dictionary<int, Test> dictionaryOfTest;
+
+            var queryA = from x in arrayOfInt
+                        select x;
+            var queryB = from x in listOfString
+                        select x;
+            var queryC = from x in listOfTest
+                        select x;
+
+            var queryD = from x in arrayOfInt
+                         where x < 5
+                         select x;
+
+            foreach (var item in queryD)
+            {
+                Console.Write(item);
+            }
+
+            var queryE = from x in arrayOfInt
+                         where x > 5
+                         select x;
+
+            foreach (var item in queryE)
+            {
+                Console.Write(item);
+            }
+
+            var queryF = from x in listOfString
+                         where x.Length > 3
+                         select x;
+
+            foreach (var item in queryF)
+            {
+                Console.Write(item);
+            }
+
+            var queryG = from x in listOfString
+                         where x.Length < 3
+                         select x;
+
+            foreach (var item in queryG)
+            {
+                Console.Write(item);
+            }
         }
     }
 
